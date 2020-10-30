f09b63b56491eb6af294878e974f81c3904df636
renanravelli <renanravellialves@gmail.com>
2020-10-30 20:06:27 -0300

    feature: adicionando page de tutor.

---
+++ src/app/page/empresa/tutor/TutorConsultar.tsx
@@ -0,0 +1,5 @@
+import React, {ReactElement} from 'react';
+
+export default function TutorConsultar(): ReactElement {
+  return <h1>Tutor Consultar</h1>
+}
---
+++ src/app/page/empresa/tutor/index.ts
@@ -0,0 +1,1 @@
+export {default as TutorRotas, TUTOR_PREFIXO} from './TutorRotas'
