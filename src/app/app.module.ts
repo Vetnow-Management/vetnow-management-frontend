94bcf2675f625b5e7dccd1d964990c03d43e017a
renan.fcamara <renan.fcamara@fornecedores.klabin.com.br>
2021-01-17 23:17:27 -0300

    Adicionando pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { LayoutModule } from './shared/layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule, InputTextModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, InputTextModule, FormsModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
