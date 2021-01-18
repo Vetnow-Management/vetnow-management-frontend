28a28e19b4a68f8d57cd5e694aabcac0392db220
renan.fcamara <renan.fcamara@fornecedores.klabin.com.br>
2021-01-18 19:50:03 -0300

    form cadastro

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, CadastroComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/pages/auth/cadastro/cadastro.component.ts
+++ src/app/pages/auth/cadastro/cadastro.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.scss']
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
+  cadastroForm: FormGroup = new FormGroup({
+    razaoSocial: new FormControl(),
+    documento: new FormControl(),
+    contato: new FormGroup({
+      email: new FormControl(),
+      telefone: new FormControl(),
+    }),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit() {
+    console.log('obj', this.cadastroForm.value);
   }
-
 }
