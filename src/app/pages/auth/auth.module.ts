47c70103e96fad394ca2bb2914c4e4d4b3810622
renan.fcamara <renan.fcamara@fornecedores.klabin.com.br>
2021-01-18 16:05:41 -0300

    adicionando pages de auth

---
+++ src/app/pages/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, CadastroComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
